8e888843beddd079d3f3f31af69ca363e0c0b7e5
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2020-12-29 18:57:30 +0545

    modified the model

---
+++ v1/transaction_log/models/account.py
@@ -0,0 +1,49 @@
+from uuid import uuid4
+
+from django.core.validators import MaxValueValidator
+from django.db import models
+from thenewboston.constants.network import BALANCE_LOCK_LENGTH, MAX_POINT_VALUE, VERIFY_KEY_LENGTH
+
+
+class TransactionLog(models.Model):
+    status_choices = [
+        ('PENDING', 0),
+        ('CONFIRMED', 1),
+        ('REJECTED', 2)
+    ]
+    sender = models.CharField(max_length=VERIFY_KEY_LENGTH)
+    recipient = models.CharField(max_length=VERIFY_KEY_LENGTH)
+    balance_lock = models.CharField(max_length=BALANCE_LOCK_LENGTH, unique=True)
+    amount = models.PositiveBigIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(MAX_POINT_VALUE)
+        ]
+    )
+    bank_account = models.CharField(max_length=VERIFY_KEY_LENGTH)
+    bank_fee = models.PositiveBigIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(MAX_POINT_VALUE)
+        ]
+    )
+    validator_account = models.CharField(max_length=VERIFY_KEY_LENGTH)
+    validator_fee = models.PositiveBigIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(MAX_POINT_VALUE)
+        ]
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    signature = models.CharField(max_length=128)
+    status = models.IntegerField(choices=status_choices, default=0)
+
+    class Meta:
+        default_related_name = 'transactionlog'
+
+    def __str__(self):
+        return (
+            f'{self.sender} '
+            f'to {self.recipient} | '
+            f'TNBC {self.amount} | '
+        )
